f0c64349ab0a3ca6b1b9c459a847d65cd9687061
Ramón Reszat <ramon@reszat.de>
2020-09-16 00:04:16 +0200

    Add mlp layers

--- autoencoders.py
+++ autoencoders.py
@@ -3,15 +3,66 @@ import tensorflow as tf
 def gaussian_MLP_encoder(x, n_hidden, n_output, keep_prob):
 
 	# define representation
+	with tf.variable_scope("gaussian_MLP_encoder"):
+        # initializers
+        w_init = tf.contrib.layers.variance_scaling_initializer()
+        b_init = tf.constant_initializer(0.)
 
-	gaussian_params = tf.matmul(h1,w) + b
+        # 1st hidden layer
+        w0 = tf.get_variable('w0', [x.get_shape()[1], n_hidden], initializer=w_init)
+        b0 = tf.get_variable('b0', [n_hidden], initializer=b_init)
+        h0 = tf.matmul(x, w0) + b0
+        h0 = tf.nn.elu(h0)
+        h0 = tf.nn.dropout(h0, keep_prob)
 
-	# free variable
-	mean = gaussian_params[:, :n_output]
-	# positive standard deviation
-	stddev = 1e-6 + tf.nn.softplus(gaussian_params[:, n_output:])
+        # 2nd hidden layer
+        w1 = tf.get_variable('w1', [h0.get_shape()[1], n_hidden], initializer=w_init)
+        b1 = tf.get_variable('b1', [n_hidden], initializer=b_init)
+        h1 = tf.matmul(h0, w1) + b1
+        h1 = tf.nn.tanh(h1)
+        h1 = tf.nn.dropout(h1, keep_prob)
+
+        # output layer
+        # borrowed from https: // github.com / altosaar / vae / blob / master / vae.py
+        wo = tf.get_variable('wo', [h1.get_shape()[1], n_output * 2], initializer=w_init)
+        bo = tf.get_variable('bo', [n_output * 2], initializer=b_init)
+
+		gaussian_params = tf.matmul(h1,wo) + bo
+
+		# free variable
+		mean = gaussian_params[:, :n_output]
+		# positive standard deviation
+		stddev = 1e-6 + tf.nn.softplus(gaussian_params[:, n_output:])
 
 	return mean, stddev
+
+def bernoulli_MLP_decoder(z, n_hidden, n_output, keep_prob, reuse=False):
+
+    with tf.variable_scope("bernoulli_MLP_decoder", reuse=reuse):
+        # initializers
+        w_init = tf.contrib.layers.variance_scaling_initializer()
+        b_init = tf.constant_initializer(0.)
+
+        # 1st hidden layer
+        w0 = tf.get_variable('w0', [z.get_shape()[1], n_hidden], initializer=w_init)
+        b0 = tf.get_variable('b0', [n_hidden], initializer=b_init)
+        h0 = tf.matmul(z, w0) + b0
+        h0 = tf.nn.tanh(h0)
+        h0 = tf.nn.dropout(h0, keep_prob)
+
+        # 2nd hidden layer
+        w1 = tf.get_variable('w1', [h0.get_shape()[1], n_hidden], initializer=w_init)
+        b1 = tf.get_variable('b1', [n_hidden], initializer=b_init)
+        h1 = tf.matmul(h0, w1) + b1
+        h1 = tf.nn.elu(h1)
+        h1 = tf.nn.dropout(h1, keep_prob)
+
+        # output layer-mean
+        wo = tf.get_variable('wo', [h1.get_shape()[1], n_output], initializer=w_init)
+        bo = tf.get_variable('bo', [n_output], initializer=b_init)
+        y = tf.sigmoid(tf.matmul(h1, wo) + bo)
+
+    return y
 
 def variational_MLP_autoencoder(x_hat, x, dim_z, n_hidden, keep_prob):
 
