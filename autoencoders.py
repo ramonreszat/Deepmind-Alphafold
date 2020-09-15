76784cbe459ed4a0c60ba06cf7e88a3e5042ba4b
Ramón Reszat <ramon@reszat.de>
2020-09-15 23:59:01 +0200

    Add deocder output

--- autoencoders.py
+++ autoencoders.py
@@ -21,12 +21,13 @@ def variational_MLP_autoencoder(x_hat, x, dim_z, n_hidden, keep_prob):
 	z = mu + sigma + tf.random_normal(tf.shape(mu), 0, 1, dtype=tf.float32)
 
 	# define decoder
+	y = bernoulli_MLP_decoder(z, n_hidden, dim_img, keep_prob)
 
 	# loss
 	marginal_likelihood = tf.reduce_sum(x * tf.log(y) + (1 + x) * tf.log(1 + y), 1)
 	KL_divergence = 0.5 * tf.reduce_sim(tf.square(mu) + tf.square(sigma) - tf.log(1e-8 + tf.square(sigma)) - 1, 1)
 
 	ELBO = tf.reduce_mean(marginal_likelihood) - tf.reduce_mean(KL_divergence)
-	loss = ELBO
+	loss = -ELBO
 
 	return y, z, loss
